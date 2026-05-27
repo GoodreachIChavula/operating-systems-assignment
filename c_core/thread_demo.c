810445dd43569214fc06483297e6e02db5aabd06
GoodreachIChavula <goodreachchavula@gmail.com>
2026-05-27 22:35:41 +0200

    Implemented threading and mutex synchronization demo

---
+++ c_core/thread_demo.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int counter = 0;
+
+pthread_mutex_t lock;
+
+void* increment_counter(void* arg) {
+
+    for(int i = 0; i < 100000; i++) {
+
+        pthread_mutex_lock(&lock);
+
+        counter++;
+
+        pthread_mutex_unlock(&lock);
+    }
+
+    return NULL;
+}
+
+int main() {
+
+    pthread_t thread1;
+
+    pthread_t thread2;
+
+
+    pthread_mutex_init(&lock, NULL);
+
+
+    pthread_create(&thread1, NULL, increment_counter, NULL);
+
+    pthread_create(&thread2, NULL, increment_counter, NULL);
+
+
+    pthread_join(thread1, NULL);
+
+    pthread_join(thread2, NULL);
+
+
+    pthread_mutex_destroy(&lock);
+
+
+    printf("Final Counter: %d\n", counter);
+
+    return 0;
+}
